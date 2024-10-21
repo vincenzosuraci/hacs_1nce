54d5b690523b4231341b34931af49d369b5c77eb
Vincenzo Suraci <suraci@diag.uniroma1.it>
2024-10-21 19:29:13 +0200

    DOMAIN = once Name = 1nce

--- custom_components/once/__init__.py
+++ custom_components/once/__init__.py
@@ -25,14 +25,14 @@ try:
         username = config_entry.data[CONF_USERNAME]
         password = config_entry.data[CONF_PASSWORD]
 
-        OnceDeviceObj = OnceDevice(params={
+        once_device = OnceDevice(params={
             "username": username,
             "password": password,
             "iccid": iccid
         })
 
         # Inizializza il coordinatore
-        coordinator = OnceCoordinator(hass, OnceDeviceObj)
+        coordinator = OnceCoordinator(hass, once_device)
 
         # Memorizza il coordinatore nel registro dei dati di Home Assistant
         hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = coordinator
